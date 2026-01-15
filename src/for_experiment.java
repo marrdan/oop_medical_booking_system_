f6151a592fe753cc200d2de0302f1d1f7302e14c
Mardan Umirbek <mardanumirbek@gmail.com>
2026-01-15 12:46:55 +0500

    Add new classes

---
+++ src/for_experiment.java
@@ -0,0 +1,11 @@
+public class for_experiment {
+    private int mardan;
+    public for_experiment(int mardan){
+        setMardan(mardan);
+    }
+
+    public void setMardan(int mardan){
+        this.mardan = mardan;
+    }
+
+}
